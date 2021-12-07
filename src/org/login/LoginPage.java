2689b940ff216884437a57bd2322426824a16757
Aarthijeganathan <aarthijegan94@gmail.com>
2021-12-07 12:34:50 +0530

    password page completed

--- src/org/login/LoginPage.java
+++ src/org/login/LoginPage.java
@@ -1,5 +1,12 @@
 package org.login;
 
-public class LoginPage {
+public class LoginPage 
+{
+	public static void passwordPage() {
+		System.out.println("password page is completed");
+}
 
+	public static void main(String[] args) {
+		passwordPage();
+	}
 }
